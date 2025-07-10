3d4994793ffbb1df9a59b02e3ece736f99424b82
ODGodinho <victor.godinho@plataforma13.com.br>
2025-07-10 18:21:09 -0300

    feat: :tada: create graylog logs project

--- @types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type * from "./ContainerInterface";
-
-export type * from "./EventsInterface";
--- tests/vitest/init.ts
+++ tests/vitest/init.ts
@@ -1,11 +1,5 @@
 import "vitest/globals.d.ts";
 
-import { container } from "./SingletonTest";
-
 export default void (async (): Promise<void> => {
-    process.env = {
-        ...process.env,
-    };
-
-    await container.setUp();
+    // Not Required
 })();
